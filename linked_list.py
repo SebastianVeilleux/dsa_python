4b7dd77e88bbad281b24521999a0dea8acfe5c6a
Sebastian Veilleux <sebastianaveilleux@gmail.com>
2025-09-10 23:01:13 -0600

    Fix comment block for middleNode function in linked list implementation

--- linked_list.py
+++ linked_list.py
@@ -109,6 +109,7 @@ class Solution:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+''''
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
@@ -126,3 +127,4 @@ class Solution:
 
         
         return slow
+'''
